bca4476a96b3bcaf77fe7ada6bb185458bb0e220
s3558127 <s3558127@student.rmit.edu.au>
2021-03-04 04:26:27 +0800

    added weather detail and reply

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,10 @@
+import java.util.*;
 public class helloworld {
 public static void main(String[] args) {
 System.out.println("Hello World!");
+Scanner sc = new Scanner(System.in);
+System.out.println("What's the weather?");
+String weather = sc.nextLine();
+System.out.println("Oh! Nice " + weather + " day!");
 }
 }
